45dd13aff6141d388e14cacf252feeb51e70bb93
Sherlock Holmes <SherlockInChina@gmail.com>
2015-01-17 11:15:21 +0800

    test1(begin to build)

---
+++ Static/js/visityData.js
@@ -0,0 +1,38 @@
+﻿var visityData={
+	'bbs':{
+		'Name':'论坛',
+		'address':'bbs/index.html',
+		'Cont':'just a bbs！<br /><br /><span >论坛！</span>',
+		'click':function(){self.location="bbs/index.html"}
+	},
+	'blog':{
+		'Name':'博客',
+		'address':'blog/index.html',
+		'Cont':'a Blog of mine！<br /><br /><span>博客!</span>',
+		'click':function(){self.location="blog/index.html"}
+	},
+	'game':{
+		'Name':'游戏',
+		'address':'game/index.html',
+		'Cont':'The Game Is On !! <br /><br /><span>游戏！</span>',
+		'click':function(){self.location="game/index.html"}
+	},
+	'material':{
+		'Name':'素材',
+		'address':'material/index.html',
+		'Cont':'就是一些素材！<br /><br /><span>素材！</span>',
+		'click':function(){self.location="material/index.html"}
+	},
+	'tool':{
+		'Name':'工具',
+		'address':'tool/index.html',
+		'Cont':'一些好用工具！<br /><br /><span>工具！</span>',
+		'click':function(){self.location="tool/index.html"}
+	},
+	'library':{
+		'Name':'图书馆',
+		'address':'library/index.html',
+		'Cont':'没事过来看看书！<br /><br /><span>图书馆！</span>',
+		'click':function(){self.location="library/index.html"}
+	}
+};
